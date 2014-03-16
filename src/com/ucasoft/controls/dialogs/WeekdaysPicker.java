309ec638d1135cb24c576206093d6da60a9f51c5
Scogun <SAntonov@UCASoft.com>
2014-03-16 23:58:05 +0400

    Add new dialog: WeekdaysPicker.

---
+++ src/com/ucasoft/controls/dialogs/WeekdaysPicker.java
@@ -0,0 +1,90 @@
+package com.ucasoft.controls.dialogs;
+
+import android.annotation.TargetApi;
+import android.app.*;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import com.ucasoft.controls.R;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.HashSet;
+
+/**
+ * Created by UCASoft.
+ * User: Antonov Sergey
+ * Date: 15.03.14
+ * Time: 16:07
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class WeekdaysPicker extends DialogFragment {
+
+    public interface OnWeekdaysListener {
+        void OnWeekdaysSelected(int[] weekdays);
+    }
+
+    private final String TAG = "WeekdaysPicker";
+    private OnWeekdaysListener listener;
+    private HashSet<Integer> selectedDays = new HashSet<Integer>();
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setMultiChoiceItems(getWeekDays(), null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        int firstDay = Calendar.getInstance().getFirstDayOfWeek();
+                        int dayOfWeek = i + firstDay;
+                        if (dayOfWeek > 7)
+                            dayOfWeek = dayOfWeek - 7;
+                        if (selectedDays.contains(dayOfWeek))
+                            selectedDays.remove(dayOfWeek);
+                        else
+                            selectedDays.add(dayOfWeek);
+                    }
+                })
+                .setTitle(R.string.weekdays_dialog_title)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (listener != null) {
+                            listener.OnWeekdaysSelected(getIntResult());
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null);
+        return builder.create();
+    }
+
+    private int[] getIntResult() {
+        int[] result = new int[selectedDays.size()];
+        int i = 0;
+        for(Integer day : selectedDays) {
+            result[i++] = day;
+        }
+        return result;
+    }
+
+    public void show(FragmentManager fragmentManager) {
+        show(fragmentManager, TAG);
+    }
+
+    public void setOnWeekdaysListener(OnWeekdaysListener listener) {
+        this.listener = listener;
+    }
+
+    private String[] getWeekDays() {
+        String[] result = new String[7];
+        String[] weekdays = DateFormatSymbols.getInstance().getWeekdays();
+        int firstDay = Calendar.getInstance().getFirstDayOfWeek();
+        for (int i = 0; i < 7; i++) {
+            result[i] = weekdays[firstDay];
+            if (firstDay == 7)
+                firstDay = 1;
+            else
+                firstDay++;
+        }
+        return result;
+    }
+}
